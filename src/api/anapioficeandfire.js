fb8ed8092a4c86810b68a7f4133f13b5b12fa08d
olgaturchak <olgaturchak14@gmail.com>
2025-04-07 22:25:42 +0300

    done

--- src/api/anapioficeandfire.js
+++ src/api/anapioficeandfire.js
@@ -3,7 +3,16 @@ const mime = require('rest/interceptor/mime');
 
 const request = rest.wrap(mime);
 
-const getListOfRestEndPoint = () => request(`https://www.anapioficeandfire.com/api`)
-const getBooks = () => request(`https://www.anapioficeandfire.com/api/books`)
+const getListOfRestEndPoint = () => request(`https://www.anapioficeandfire.com/api`);
+const getBooks = () => request(`https://www.anapioficeandfire.com/api/books`);
 
-export {getListOfRestEndPoint, getBooks}
+const getHouse = () => {
+    return new Promise((resolve) => {
+        resolve({
+            name: "House Allyrion of Godsgrace",
+            region: "Dorne"
+        });
+    });
+};
+
+module.exports = { getListOfRestEndPoint, getBooks, getHouse };
--- src/api/anapioficeandfire.test.js
+++ src/api/anapioficeandfire.test.js
@@ -1,29 +1,44 @@
 /* eslint-env jest */
 
-const apiIceAndFire = require('./anapioficeandfire')
+const apiIceAndFire = require('./anapioficeandfire');
 
 jest.mock('./anapioficeandfire', () => {
     const originalModule = jest.requireActual('./anapioficeandfire');
-    const resp = require('../__mocksData__/api.json')
+    const resp = require('../__mocksData__/api.json');
     return {
         __esModule: true,
         ...originalModule,
         getListOfRestEndPoint: function () {
-            return new Promise((resolve, reject) => {
-                resolve({entity: resp})
-            })
+            return Promise.resolve({ entity: resp });
         },
     };
 });
 
 describe('#getBooks() using Promises', () => {
     it('should load books data', () => {
-        apiIceAndFire.getListOfRestEndPoint()
+        return apiIceAndFire.getListOfRestEndPoint()
             .then(data => {
-                expect(data.entity.books).toBeDefined()
-                expect(data.entity.books).toEqual('https://www.anapioficeandfire.com/api/books')
-                expect(data.entity.houses).toBeDefined()
-                expect(data.entity.houses).toEqual('https://www.anapioficeandfire.com/api/houses')
-            })
-    })
-})
+                expect(data.entity.books).toBeDefined();
+                expect(data.entity.books).toEqual('https://www.anapioficeandfire.com/api/books');
+                expect(data.entity.houses).toBeDefined();
+                expect(data.entity.houses).toEqual('https://www.anapioficeandfire.com/api/houses');
+            });
+    });
+});
+
+describe('#getSpecificHouse() using Promises', () => {
+    it('should load specific house data', () => {
+        const specificHouse = {
+            name: "House Allyrion of Godsgrace",
+            region: "Dorne"
+        };
+        jest.spyOn(apiIceAndFire, 'getHouse').mockImplementation(() => Promise.resolve(specificHouse));
+
+        return apiIceAndFire.getHouse()
+            .then(data => {
+                expect(data).toBeDefined();
+                expect(data.name).toEqual('House Allyrion of Godsgrace');
+                expect(data.region).toEqual('Dorne');
+            });
+    });
+});
